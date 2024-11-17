f147ae8224fef851f1c071fda90dd1b054452cc7
hammer-cjj <cf512823985@gmail.com>
2024-11-17 20:23:49 +0800

    Add list command for listing tasks

--- src/main/java/com/cf/HandleCommand.java
+++ src/main/java/com/cf/HandleCommand.java
@@ -2,6 +2,8 @@ package com.cf;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Copyright(C) 2024- com.cf
@@ -16,38 +18,70 @@ public class HandleCommand {
      * Execute command
      */
     public static void execute(String input) {
-        // Add task
-        if (input.startsWith("task-cli add ")) {
+        if (input.startsWith("task-cli add ")) { // Add task
             String arg = input.replace("task-cli add ", "");
-            if (arg.startsWith("\"") && arg.endsWith("\"")) {
-                arg = arg.substring(1, arg.length() - 1);
-                if (arg.isEmpty() || arg.contains("\"")) {
-                    throw new TaskException("Error: invalid argument task-cli add");
-                }
-            }
-
-            DataEntity dataEntity;
-            // Load data from a json file
-            try {
-                dataEntity = TaskJSONFileUtil.readFromJSONFile();
-            } catch (IOException e) {
-                throw new TaskException("Error: reading from a JSON file: " + e);
-            }
-
-            Task task = new Task(
-                    arg,
-                    TaskStatus.TODO,
-                    LocalDateTime.now(),
-                    LocalDateTime.now());
-
-            try {
-                TaskJSONFileUtil.writeToJSONFile(dataEntity, task);
-            } catch (IOException e) {
-                throw new TaskException("Error: writing to a JSON file: " + e);
-            }
+            addCommand(arg);
+        } else if ("task-cli list".equals(input)) { // Listing all tasks
+            listCommand(null);
+        } else if ("task-cli list done".equals(input)) {
+            listCommand("done");
+        } else if ("task-cli list todo".equals(input)) {
+            listCommand("todo");
+        } else if ("task-cli list in-progress".equals(input)) {
+            listCommand("in-progress");
         } else {
             System.err.println("Usage: task-cli <command> <arguments>");
             System.err.println("Please enter q to quit.");
+        }
+    }
+
+    private static void addCommand(String arg) {
+        if (arg.startsWith("\"") && arg.endsWith("\"")) {
+            arg = arg.substring(1, arg.length() - 1);
+            if (arg.isEmpty() || arg.contains("\"")) {
+                throw new TaskException("Error: invalid argument task-cli add");
+            }
+        }
+
+        DataEntity dataEntity;
+        // Load data from a json file
+        try {
+            dataEntity = TaskJSONFileUtil.readFromJSONFile();
+        } catch (IOException e) {
+            throw new TaskException("Error: reading from a JSON file: " + e);
+        }
+
+        Task task = new Task(
+                arg,
+                TaskStatus.TODO,
+                LocalDateTime.now(),
+                LocalDateTime.now());
+
+        try {
+            TaskJSONFileUtil.writeToJSONFile(dataEntity, task);
+        } catch (IOException e) {
+            throw new TaskException("Error: writing to a JSON file: " + e);
+        }
+    }
+
+    private static void listCommand(String arg) {
+        DataEntity dataEntity;
+        // Load data from a json file
+        try {
+            dataEntity = TaskJSONFileUtil.readFromJSONFile();
+        } catch (IOException e) {
+            throw new TaskException("Error: reading from a JSON file: " + e);
+        }
+        if (dataEntity != null) {
+            List<Task> tasks = dataEntity.getTasks();
+            if (arg == null) {
+                tasks.forEach(System.out::println);
+            } else  {
+                List<Task> collect = tasks.stream()
+                        .filter(item -> item.getStatus().getMsg().equals(arg))
+                        .toList();
+                collect.forEach(System.out::println);
+            }
         }
     }
 }
--- src/main/java/com/cf/Main.java
+++ src/main/java/com/cf/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Task Tracker programme");
+        System.out.println("*****Welcome to Task Tracker programme*****");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
